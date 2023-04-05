b0010da66b4c822aa96cecf38f80428d38a58e63
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-05 20:08:24 +0300

    start

---
+++ src/js/task-01.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*const categories = document.querySelector('#categories');
+const item = document.querySelectorAll('item');
+console.log(item.length);
+
+categories.forEach(item => console.log(item.textContent));*/
+console.log(document);
+
+
+const body = document.body;
+console.log(body);
+const categories = document.querySelector('#categories');
+console.log(categories);
+const item = categories.querySelectorAll('item');
+console.log(item);
