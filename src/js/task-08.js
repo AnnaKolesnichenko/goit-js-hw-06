d2e009897174904c58287df0ad73edd97a7d5ac2
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-09 17:49:18 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,31 @@
 const loginForm = document.querySelector('.login-form');
+const submitBtn = document.querySelector('button');
+
+
+function onSubmit(event) {
+    event.preventDefault();
+    /*const formData = new FormData(event.currentTarget);
+    console.log(formData);
+
+    formData.forEach((input, name) => {
+        console.log(name + ':' + input);
+    })*/
+
+    const formElems = event.currentTarget.elements;
+    const email = formElems.email.value;
+    const password = formElems.password.value;
+
+    if(email === '' || password === '') {
+        alert('Please fill in the necessary fields');
+    } 
+    const form = {
+        email: email,
+        password: password
+    }
+
+    console.log(form);
+    event.currentTarget.reset();
+
+}
+
+loginForm.addEventListener('submit', onSubmit);
