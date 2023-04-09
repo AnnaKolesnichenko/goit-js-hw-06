30dfd08b101692872e8ab5c833afe1d3d876ee53
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-09 18:21:58 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,22 @@
-function getRandomHexColor() {
+
+const widget = document.querySelector('.widget');
+const colorTitle = document.querySelector('.color');
+const changeClrBtn = document.querySelector('.change-color');
+const colorByName = document.body.getAttribute('style');
+
+console.dir()
+
+
+const coloring = function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+function onColorChange() {
+  document.body.style.backgroundColor = coloring();
+  colorTitle.textContent = coloring();
+}
+
+changeClrBtn.addEventListener('click', onColorChange);
