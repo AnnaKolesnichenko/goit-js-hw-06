ce28551cb9b773c561bc4c07500fa14951cf55ee
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-14 21:26:08 +0300

    Task7Updates

--- js/task-07.js
+++ js/task-07.js
@@ -1,17 +1,18 @@
 const sizeControlInput = document.querySelector('#font-size-control');
 const anyTextSpan = document.querySelector('#text');
 
-//const minFont = sizeControlInput.getAttribute('min');
-//const maxFont = sizeControlInput.getAttribute('max');
-//const minNum = Number(minFont);
-//const maxNum = Number(maxFont);
+const minFont = sizeControlInput.getAttribute('min');
+const maxFont = sizeControlInput.getAttribute('max');
+const minNum = Number(minFont);
+const maxNum = Number(maxFont);
 
-function onFontSizeChange(event) {   
-    anyTextSpan.style.fontSize = `${event.currentTarget.value}px`;
+
+function onFontSizeChange(event) {       
+    anyTextSpan.style.fontSize = `${event.target.value}px`;
 
     //const fontSizePx = Math.random() * (maxNum - minNum) + minNum;
     //anyTextSpan.style.fontSize = fontSizePx + 'px';    
 }
 
-sizeControlInput.addEventListener('change', onFontSizeChange);
+sizeControlInput.addEventListener('input', onFontSizeChange);
 
--- js/task-09.js
+++ js/task-09.js
@@ -5,16 +5,19 @@ const changeClrBtn = document.querySelector('.change-color');
 const colorByName = document.body.getAttribute('style');
 
 
-const coloring = function getRandomHexColor() {
+function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
+//function onColorChange() {
+  //document.body.style.backgroundColor = getRandomHexColor();
+  //colorTitle.textContent = getRandomHexColor();
 
-function onColorChange() {
-  document.body.style.backgroundColor = coloring();
-  colorTitle.textContent = coloring();
-}
+//}
 
-changeClrBtn.addEventListener('click', onColorChange);
+changeClrBtn.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorTitle.textContent = getRandomHexColor();
+});
