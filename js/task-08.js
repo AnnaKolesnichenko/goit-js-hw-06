902e0216f353ac0551879d800764f1d8e697552d
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-15 15:28:27 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -10,22 +10,19 @@ function onSubmit(event) {
     formData.forEach((input, name) => {
         console.log(name + ':' + input);
     })*/
+    const {elements: {email, password}} = event.currentTarget;
 
-    const formElems = event.currentTarget.elements;
-    const email = formElems.email.value;
-    const password = formElems.password.value;
+    //const formElems = event.currentTarget.elements;
+    //const email = formElems.email.value;
+    //const password = formElems.password.value;
 
-    if(email === '' || password === '') {
+    if(email.value === '' || password.value === '') {
         alert('Please fill in the necessary fields');
+        return;
     } 
-    const form = {
-        email: email,
-        password: password
-    }
-
-    console.log(form);
+    
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
     event.currentTarget.reset();
-
 }
 
 loginForm.addEventListener('submit', onSubmit);
